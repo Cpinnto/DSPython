32d5783482dda6ebeca8d9de44dd00b043d50b1b
Cpinnnto <tu_email_de_github>
2025-08-27 19:37:06 -0500

    This is a try just to make sure that I sync both vs and my github

--- greeting.py
+++ greeting.py
@@ -4,3 +4,15 @@
 
 
 print("Hello Python DS Developers!")
+
+for i in range(5):
+    if i == 1: 
+        print("Esto")
+    elif i == 2:
+        print("Es")
+    elif i == 3:
+        print("una")
+    elif i == 4:
+        print("Prueba")
+    else:
+        print("!!!!!")
